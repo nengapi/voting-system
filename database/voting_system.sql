e04a5ac682a4b6a920b863f8c692b30759b33825
nengapi <apichet612@gmail.com>
2021-12-15 08:04:50 +0700

    add blockchain database

--- database/voting_system.sql
+++ database/voting_system.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Dec 14, 2021 at 11:32 PM
+-- Generation Time: Dec 15, 2021 at 02:03 AM
 -- Server version: 10.4.21-MariaDB
 -- PHP Version: 8.0.11
 
@@ -24,21 +24,59 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `block`
+--
+
+CREATE TABLE `block` (
+  `BlockID` bigint(20) NOT NULL,
+  `prevBlockID` bigint(20) DEFAULT NULL,
+  `voterID` bigint(20) NOT NULL,
+  `candidate` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `messageHash` varchar(255) NOT NULL,
+  `messagePrevHash` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `block`
+--
+
+INSERT INTO `block` (`BlockID`, `prevBlockID`, `voterID`, `candidate`, `created_at`, `messageHash`, `messagePrevHash`) VALUES
+(3, NULL, 3, 1, '2021-12-15 00:56:41', '5feceb66ffc86f38d952786c6d696c79c2dbc239dd4e91b46729d73a27fb57e9', NULL);
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `id` bigint(20) NOT NULL,
   `student_id` bigint(20) NOT NULL,
-  `password` varchar(20) NOT NULL,
+  `password` varchar(255) NOT NULL,
   `fullName` varchar(255) NOT NULL,
   `isAdmin` tinyint(1) DEFAULT 0,
   `created_at` timestamp NULL DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `student_id`, `password`, `fullName`, `isAdmin`, `created_at`) VALUES
+(3, 1234, '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', 'neng', 0, '2021-12-14 23:15:22');
+
+--
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `block`
+--
+ALTER TABLE `block`
+  ADD PRIMARY KEY (`BlockID`),
+  ADD KEY `voterID` (`voterID`),
+  ADD KEY `prevBlockID` (`prevBlockID`);
 
 --
 -- Indexes for table `users`
@@ -52,10 +90,27 @@ ALTER TABLE `users`
 --
 
 --
+-- AUTO_INCREMENT for table `block`
+--
+ALTER TABLE `block`
+  MODIFY `BlockID` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+  MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `block`
+--
+ALTER TABLE `block`
+  ADD CONSTRAINT `prevBlockID` FOREIGN KEY (`prevBlockID`) REFERENCES `block` (`BlockID`) ON DELETE CASCADE,
+  ADD CONSTRAINT `voterID` FOREIGN KEY (`voterID`) REFERENCES `users` (`id`) ON DELETE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
